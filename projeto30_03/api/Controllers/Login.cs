035b74709d103554cfe695b8e37d4d4a37832d41
eduandrade2903 <102692848+eduandrade2903@users.noreply.github.com>
2022-03-31 21:54:17 -0300

    Add files via upload

---
+++ projeto30_03/api/Controllers/Login.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Model;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Login:ControllerBase
+    {
+        
+        [HttpGet("teste/{valor}")]
+        public object teste(double valor)
+        {
+            return new {
+                nome = "Eduardo",
+                idade = valor
+            };
+
+        }
+
+        [HttpGet("login/{login}/{senha}")]
+
+        public object RealizarLogin(String login, String senha )
+        {
+            Usuario user = new Usuario(login, senha);
+
+
+            user.search(user.getLogin());
+
+            return new{
+                login = user.getLogin(),
+                senha = user.getSenha()
+            };
+        }
+            
+
+    }
+}
